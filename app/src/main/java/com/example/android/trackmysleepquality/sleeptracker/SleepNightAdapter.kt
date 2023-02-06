14d4f5d79deea36a26d5ed7e0b1d109db629b701
ASZhartun <as.zhartun@gmail.com>
2023-02-06 09:54:52 +0200

    feature: add adapter with custom view for recycler and rewrite onBindViewHolder()

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.*
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
     var data = listOf<SleepNight>()
         set(value) {
             field = value
@@ -17,14 +20,36 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.nightQuality.toString()
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text =
+            convertDurationToFormatted(item.startNightInMs, item.endNightInMs, res)
+
+        holder.qualityString.text = convertNumericQualityToString(item.nightQuality, res)
+
+        holder.qualityImage.setImageResource(
+            when (item.nightQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+        )
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = inflater.inflate(R.layout.list_item_sleep_night, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+        val qualityString: TextView = itemView.findViewById(R.id.quality_string)
+        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
     }
 }
